67d9edc224bc7b259920b9e8093ae8d8c5607839
ijeliazkov <ijeliazkov@intoxitrack.net>
2015-07-02 15:56:36 -0500

    Adding an example blob

---
+++ test/spec/directives/simpleknob.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: simpleKnob', function () {
+
+  // load the directive's module
+  beforeEach(module('knobyApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<simple-knob></simple-knob>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the simpleKnob directive');
+  }));
+});
