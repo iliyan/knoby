d088b826b563e0839c6857c8d995ef286af9acfc
iliyan jeliazkov <iliyan@acm.org>
2015-08-02 23:45:17 -0500

    Changing draggable to use the more generic transformation matrix

--- app/scripts/directives/draggable.js
+++ app/scripts/directives/draggable.js
@@ -9,26 +9,41 @@
 angular.module('knobyApp')
   .directive('draggable', ['$document', function ($document) {
     return function (scope, element, attr) {
-      var startX = 0, startY = 0, x = 0, y = 0;
+      var startX = 0, startY = 0;
+      var clazz;
+      var tranformation;
       element.on('mousedown', function (event) {
         // Prevent default dragging of selected content
         event.preventDefault();
-        startX = event.screenX - x;
-        startY = event.screenY - y;
+        tranformation = element.attr("transform").slice(7,-1).split(' ');
+        for(var i=0; i<tranformation.length; i++) {
+          tranformation[i] = parseFloat(tranformation[i]);
+        }
+
+        scope.isProcreating = true;
+        startX = event.screenX - tranformation[4];
+        startY = event.screenY - tranformation[5];
+
+        clazz = element.attr('class');
+        element.attr('class', 'dragging');
+
         $document.on('mousemove', mousemove);
         $document.on('mouseup', mouseup);
       });
 
       function mousemove(event) {
-        y = event.screenY - startY;
-        x = event.screenX - startX;
-        element.css({
-          top: y + 'px',
-          left: x + 'px'
-        });
+
+        tranformation[4] = event.screenX - startX;
+        tranformation[5] = event.screenY - startY;
+
+        var moved = "matrix(" + tranformation.join(' ') + ")";
+        element.attr("transform", moved);
+
       }
 
       function mouseup() {
+        scope.isProcreating = false;
+        element.attr('class', clazz);
         $document.off('mousemove', mousemove);
         $document.off('mouseup', mouseup);
       }
